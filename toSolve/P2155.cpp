0bfa1652857cb397bf903dcea23aae5013523fb6
mklzc <mklzc102@qq.com>
2022-06-04 13:34:22 +0800

    2022.6.4

--- toSolve/P2155.cpp
+++ toSolve/P2155.cpp
@@ -2,6 +2,14 @@
 // Author: lzc
 #include <bits/stdc++.h>
 using namespace std;
+int T, R;
+inline void rai()
+{
+    scanf("%d %d", &T, &R);
+    while (T--)
+    {
+    }
+}
 int main()
 {
     return 0;
