6b1d8af6559697f4a669e59071efb69ab8c76b98
mklzc <mklzc102@qq.com>
2022-06-07 23:41:17 +0800

    2022.6.7

---
+++ toSolve/P2216.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, b, n;
+inline int rai()
+{
+    scanf("%d %d %d", &a, &b, &n);
+    for (int i = 1; i <= a; i++)
+        for (int j = 1; j <= b; j++)
+        {
+        }
+    return 0;
+}
+int main()
+{
+    return rai();
+}
---
+++ toSolve/P6583.cpp
@@ -0,0 +1,14 @@
+// url: https://www.luogu.com.cn/problem/P6583
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+LL n;
+void init()
+{
+}
+inline int raw()
+{
+    scanf("%lld", &n);
+    return 0;
+}
+int main() {return raw();}
